d5402473b947bc64389ac215c08c0e3b60413228
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-23 21:58:28 +0100

    added function chooseTwoRandomGirls()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'babe-smash';
+  random_a = 0;
+  random_b = 0;
+
+  chooseTwoRandomGirls(){
+    let total_girls = 2;
+    this.random_a = Math.floor((Math.random() * total_girls) + 1);
+	  this.random_b = Math.floor((Math.random() * total_girls) + 1);
+
+    while(this.random_a == this.random_b){
+      this.random_b = Math.floor((Math.random() * total_girls) + 1);
+    } 
+  }
 
   voteForGirl(girl_chosen){
     console.log(girl_chosen);
+  }
+
+  calculateRatings() {
+    
+
+
+
   }
 }
